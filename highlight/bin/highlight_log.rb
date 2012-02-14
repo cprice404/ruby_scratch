ffba6f7d9c0cb12f4631ca6bf1453a4efc525865
cprice <chris@digitalnotions.com>
2012-02-13 21:04:58 -0800

    a stupid little program that parse a text file, searches for a list of patterns, highlights them as html and saves out an html file

---
+++ highlight/bin/highlight_log.rb
@@ -0,0 +1,7 @@
+require "../lib/highlighter"
+Highlighter.highlight("../log.txt",
+                      {
+                          "Daniel Pittman" => "#0000FF",
+                          "Jacob Helwig" => "#FF0000",
+                          "Nick Lewis" => "#00FF00",
+                      })
